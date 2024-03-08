1d30ceaf04e049eec5d3b42cd96179329e4a2ac5
Ryan Romero <raromero103@gmail.com>
2024-03-07 16:32:59 -0800

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Python CRUD Demo
+Creating an API using the flask library
+
+## To Run
+Install flask
+```
+pip3 install flask
+```
---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/get/<var>")
+def get_data(var):
+    dummy_data = {
+        "id": var,
+        "name": "user",
+        "email": "e@mail.com"
+    }
+    
+    optional = request.args.get("optional")
+    if optional:
+        dummy_data[optional] = True
+        
+    return jsonify(dummy_data), 200
+
+if __name__ == "__main__":
+    app.run(debug=True)
