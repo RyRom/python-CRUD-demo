22c7c16521e832d2ef953cbaf3182246bd130f67
Ryan Romero <raromero103@gmail.com>
2024-03-09 18:17:37 -0800

    Reverted old.py

--- old.py
+++ old.py
@@ -4,28 +4,9 @@ from flask_login import UserMixin
 
 app = Flask(__name__)
 
-db = SQLAlchemy()
-app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///database.db'
-app.config['SECRET_KEY'] = 'mysecretkey'    
-db.init_app(app)
-db.create_all()
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False) 
-
 @app.route("/")
 def home():
-    return render_template('home.html')
-
-@app.route("/login")
-def login():
-    return render_template('login.html')
-
-@app.route("/register")
-def register():
-    return render_template('register.html')
+    return "Home"
 
 @app.route("/get/<var>")
 def get_data(var):
